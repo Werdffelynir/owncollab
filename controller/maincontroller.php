4326bf0c518a6bcd1231766f3f84d09d2c0db523
bogdan_p <bohdan.pidoprygora@gmail.com>
2016-06-14 14:54:34 +0300

    logo fixed

--- controller/maincontroller.php
+++ controller/maincontroller.php
@@ -166,15 +166,17 @@ class MainController extends Controller {
 		$statistic['all_groups'] = count($this->connect->groups->get());
 		$statistic['tasks_progress'] = 100*$statistic['tasks_finished']/$statistic['all_tasks'];
 		$statistic['talks_progress'] = 100*$statistic['participating_talks']/$statistic['all_talks'];
-
+		$projectName = $this->connect->task()->getById(1);
+		$result['logo_src'] = Helper::prevImg($result['logo']);
 		$params = [
 			'sub_url' => '',
 			'body_url' => '',
 			'current_user' => $this->userId,
 			'current_val' =>$result,
-			'statistic' =>$statistic
+			'statistic' =>$statistic,
+			'projectName' =>$projectName['text']
 		];
-		$projectName = $this->connect->task()->getById(1);
+		
 		
 		$params['sub_url'] = urldecode("Help me working in my project ".$projectName['text']);
 		$params['body_url'] = urldecode("Hi there, currently I am working on the project ".$projectName['text']." and I will need some help with it. Please let me know, if you have resources to join the project.");
