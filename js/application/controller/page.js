36dd74b71e7f8b223e56b9da8ef0904d3a2d35d8
bogdan_p <bohdan.pidoprygora@gmail.com>
2016-06-14 18:03:49 +0300

    finish logo

--- js/application/controller/page.js
+++ js/application/controller/page.js
@@ -38,7 +38,7 @@ if (App.namespace) {
                         var file = r[0];
                         $(".uploadedfiles ul").append('<li>' + file.name + ' ' + sizeDefinitionString(file.size) + '<input type="hidden" name="upload-files[]" value="' + file.id + '"></li>');
                         var usr_name = $('head').attr('data-user');
-                        $("#project_logo").attr('data-src', usr_name + '/files/ProjectLogo/' + file.name);
+                        // $("#project_logo").attr('data-src', usr_name + '/files/ProjectLogo/' + file.name);
                         App.Action.Api.request('logoBaseEncode', function (response) {
                             if (response.requesttoken) {
                                 app.requesttoken = response.requesttoken;
@@ -50,7 +50,7 @@ if (App.namespace) {
                         //вставка изображений
                         $('.uploadedfiles>ul>li').click(function (event) {
                             event.preventDefault();
-                            $("#project_logo").attr('data-src', usr_name + '/files/ProjectLogo/' + file.name);
+                            // $("#project_logo").attr('data-src', usr_name + '/files/ProjectLogo/' + file.name);
                             App.Action.Api.request('logoBaseEncode', function (response) {
                                 if (response.requesttoken) {
                                     app.requesttoken = response.requesttoken;
@@ -89,7 +89,7 @@ if (App.namespace) {
                                     event.preventDefault();
                                     var usr_name = $('head').attr('data-user');
                                     var logo_name = $(this).html();
-                                    $("#project_logo").attr('data-src',  usr_name + '/files/ProjectLogo/' + logo_name);
+                                    // $("#project_logo").attr('data-src',  usr_name + '/files/ProjectLogo/' + logo_name);
                                     App.Action.Api.request('logoBaseEncode', function (response) {
                                         if (response.requesttoken) {
                                             app.requesttoken = response.requesttoken;
@@ -129,7 +129,7 @@ if (App.namespace) {
             $("form").submit(function (event) {
                 event.preventDefault();
                 var form = Util.formData(this, true);
-                form.logo = jQuery('#project_logo').attr('data-src');
+                form.logo = jQuery('#project_logo').attr('src');
                 App.Action.Api.request('saveall', function (response) {
                     if (response.requesttoken) {
                         App.requesttoken = response.requesttoken;
